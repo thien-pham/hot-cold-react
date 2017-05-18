a7d6185f81bd5575335916ebb1108d339422ba6c
pham-thien <pham.thien@outlook.com>
2017-05-18 13:26:34 -0400

    initial commit

--- src/components/game.js
+++ src/components/game.js
@@ -5,14 +5,32 @@ import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
-}
+export default class Game extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state=({
+            guesses: [],
+            feedback: 'Make your guess!',
+            count: 0
+        })
+    }
+
+    updateGuess(e){
+        this.setState({guesses: e});
+    }
+
+
+    render(){
+    
+        return (
+            <div>
+                <Header />
+                <GuessSection feedback={this.state.feedback} onSubmit={(e)=>{this.state.updateGuess(e)}}
+                    ref={input => this.input=input}/>
+                <GuessCount count={this.state.count} />
+                <GuessList guesses={this.state.guesses} />
+            </div>
+        );
+    }
+}
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -2,14 +2,24 @@ import React from 'react';
 
 import './guess-form.css';
 
-export default function GuessForm(props) {
-    return (
-        <form>
-            <input type="text" name="userGuess" id="userGuess"
-                className="text" maxLength="3" autoComplete="off"
-                placeholder="Enter your Guess" required />
-            <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
-        </form>
-    );
-};
+export default class GuessForm extends React.Component {
+
+    onSubmit(e){
+        e.preventDefault();
+        const capturedText = this.input.value; 
+        console.log(capturedText);
+    }
+    
+    render() {
+        return (
+                <form onSubmit={e=>{this.onSubmit(e)}}>
+                    <input type="text" name="userGuess" id="userGuess"
+                        className="text" maxLength="3" autoComplete="off"
+                        placeholder="Enter your Guess" required ref={input => this.input=input}/>
+                    <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
+                </form>
+            );
+        };
+    }
+    
 
