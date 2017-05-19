bf5d354171341001fb27741116f6343865c50da4
pham-thien <pham.thien@outlook.com>
2017-05-19 01:00:57 -0400

    captured guess!

--- src/components/game.js
+++ src/components/game.js
@@ -12,15 +12,31 @@ export default class Game extends React.Component {
         this.state=({
             guesses: [],
             feedback: 'Make your guess!',
-            count: 0
+            answer: Math.floor(Math.random() * 100) + 1 
         })
         this.updateGuess = this.updateGuess.bind(this);
     }
     
-    updateGuess(e) {
-        e.preventDefault();
-        console.log(e.target.value, 'testing');
-        this.setState({guesses:[...this.state.guesses, e.target.value]
+    updateGuess(guess) {
+        let feedback;
+        const difference = Math.abs(guess - this.state.answer);
+        
+        if(guess === this.state.answer){
+            feedback='You won! Click new game to play again'
+        }
+        else if(difference <= 10){
+            feedback='hot';
+        }
+        else if (difference <= 25){
+            feedback='warm';
+        }
+        else {
+            feedback='cold';
+        }
+        
+
+        console.log(guess, 'testing');
+        this.setState({feedback, guesses:[...this.state.guesses, guess]
         });
     }
 
@@ -30,10 +46,10 @@ export default class Game extends React.Component {
             <div>
                 <Header />
                 <GuessSection feedback={this.state.feedback} test={this.updateGuess}
-                              onChangeValue={this.updateGuess}  />
+                              handleSubmit={guess=>this.updateGuess(guess)}  />
                     
-                <GuessCount count={this.state.count} />
-                {/*<GuessList guesses={this.state.guesses} />*/}
+                <GuessCount count={this.state.guesses.length} />
+                <GuessList guesses={this.state.guesses} />
             </div>
         );
     }
--- src/components/guess-section.js
+++ src/components/guess-section.js
@@ -3,7 +3,17 @@ import React from 'react';
 import './guess-section.css';
 
 export default class GuessSection extends React.Component {
-
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleSubmit(e) {
+        e.preventDefault();
+        let value = this.input.value;
+        console.log('hi' + value);
+        this.props.handleSubmit(value);
+    }
 
     render() {
     return (
@@ -11,13 +21,13 @@ export default class GuessSection extends React.Component {
             <h2 id="feedback">{this.props.feedback}</h2>
             
     
-            <form >    
+            <form onSubmit={e=>this.handleSubmit(e)}>    
                 <input type="text" name="userGuess" id="userGuess"
                     className="text" maxLength="3" autoComplete="off"
                     placeholder="Enter your Guess" required ref={input => this.input=input} 
                     />
                 <input type="submit" id="guessButton" className="button" name="submit" value="Guess"
-                onSubmit={this.props.onChangeValue}/>
+                />
             </form>
 
         </section>
